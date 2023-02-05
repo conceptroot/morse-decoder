faaa2123291c0a15b0baffdaa0a4f41793e0d1b0
conceptroot <conceptroot@gmail.com>
2023-02-06 01:04:12 +0530

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -38,9 +38,37 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    // console.log(`expr = "${expr}"`)
+    let byteCharList = expr.match(/.{1,10}/g)
+    // console.log(`🥭 byteCharList = ${byteCharList}`)
+    let result = byteCharList.map(morseChar => decodeChar(morseChar) )
+    result = result.join('')
+    console.log(`🔥 And result is: ${result}`)
+    return result
 }
 
 module.exports = {
     decode
+}
+
+const decodeChar = code => {
+    // console.log('='.repeat(40))
+    // console.log('🔨 code =', code)
+    let resultList = [] 
+    // проверяем на пробел
+    if (code[0] === "*") return " "
+    // делим код на порции из 2х цифер
+    let codePartList = code.match(/.{2}/g)
+    // console.log(`codePartList = ${codePartList}`)
+    // парсим 10 или 11
+    codePartList.forEach(codePart => {
+        if (codePart === "10") resultList.push('.')
+        else if (codePart === "11") resultList.push('-')
+        else if (codePart === "00") { return }
+    })
+    let result = resultList.join('')
+    // console.log(`1️⃣ result = ${result}`)
+    result = MORSE_TABLE[result]
+    // console.log(`2️⃣result = ${result}`)
+    return result
 }
